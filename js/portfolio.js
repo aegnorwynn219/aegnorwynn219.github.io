d97928f13dcaabb960d00e2c1d87e5c326245ffd
aegnorwynn219 <70857984+aegnorwynn219@users.noreply.github.com>
2020-09-12 10:09:25 +0200

    Continued work on bio and projects

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,13 +1,13 @@
 new Swiper(".swiper-container", {
   speed: 400,
   spaceBetween: 100,
-  effect: "slide",
+  effect: "coverflow",
   slidesPerView: 1,
   loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  }
+  },
 });
 
 // navigation physics
